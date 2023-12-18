bb39985c5ffd1142d135a0947e21b06fa6bee0da
Arina <arinacypcova@gmail.com>
2023-12-18 19:07:41 +0700

    Added Sem_2

---
+++ Seminar_2/java/SuperMarket/src/Interfaces/iQueueBehaviour.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+import Classes.Actor;
+
+public interface iQueueBehaviour {
+    public void takeInQueue(iActorBehaviour actor);
+    public void releaseFromQueue();
+    public void takeOrder();
+    public void giveOrder ();
+}
